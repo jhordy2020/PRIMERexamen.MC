347d547199dd3f74a6ee3ddb37887761de38b012
jhordy2020 <200702jhordy@gmail.com>
2024-11-12 09:37:14 -0500

    Add files via upload

---
+++ sistema/claveequivocada.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1></h1>
+    <br>
+    SE INGRESO DATOS NO VALIDOS VOLVER AL INICIO
+    <br>
+    <a href="http://127.0.0.1/sistema">volver al login</a>
+</body>
+</html>
---
+++ sistema/models/conect/conexion.php
@@ -0,0 +1,19 @@
+<?
+
+class conexion {
+    //public
+
+    //protegida
+
+    //private
+    private $host;
+    private $namedb;
+    private $userdb;
+    private $passwordb;
+    private $charset;
+
+
+}
+
+
+?>
